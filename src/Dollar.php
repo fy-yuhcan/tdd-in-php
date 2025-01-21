6bf6daff62437b35dd1933fbb14279573815cfa1
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-01-21 13:34:20 +0900

    最小限テストを通すためにDollarクラスを作成

---
+++ src/Dollar.php
@@ -0,0 +1,15 @@
+<?php
+class Dollar
+{
+    public $amount;
+
+    public function __construct($amount)
+    {
+        //空実装
+    }
+
+    public function times($multiplier)
+    {
+        //空実装
+    }
+}
