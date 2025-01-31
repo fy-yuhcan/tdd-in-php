f1715fbcae33ee7316ba7fb47d070000ff629f3c
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-01-31 18:10:25 +0900

    pairクラスを使ってキーとして扱う

--- src/Bank.php
+++ src/Bank.php
@@ -1,9 +1,12 @@
 <?php
 
 require_once __DIR__ . '/Expression.php';
+require_once __DIR__ . '/Pair.php';
 
 class Bank
 {
+    private array $rates = [];
+
     public function reduce($source, $to)
     {
         return $source->reduce($this,$to);
@@ -11,8 +14,11 @@ class Bank
 
     //testReduceMoneyDifferentCurrencyテストを通すため空実装
     public function addRate($from,$to,$rate)
-    {  
-          
+    { 
+        $pair = new Pair($from,$to); 
+        $key = (string)$pair;
+
+        $this-> rates[$key] = $rate;
     }
 
     //bankで為替レートの計算をする
