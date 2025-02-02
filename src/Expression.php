3839c3422cd350458b826d2cdba505837278dcaf
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-02 14:28:37 +0900

    expressionにplusメソッドを追加する

--- src/Expression.php
+++ src/Expression.php
@@ -3,6 +3,7 @@
 //PHP の動的ディスパッチによりインスタンスがmoneyならMoney::reduce($to)、SumならSum::reduce($to)が呼ばれる
 interface Expression
 {
+    public function plus(Expression $addend);
     //phpでは$thisは使えない（予約された参照だから)
     public function reduce(Bank $bank,$to);
 }
--- src/Sum.php
+++ src/Sum.php
@@ -20,4 +20,9 @@ class Sum implements Expression
         $amount = $this->augend->reduce($bank,$to)->amount + $this->addend->reduce($bank,$to)->amount;
         return new Money($amount, $to);
     }
+
+    public function plus(Expression $addend): Expression
+    {
+        return null;
+    }
 }
